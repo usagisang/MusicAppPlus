40367e948851e2304314f405c78b32bfc10071f4
Liu Zhengjie <1023192250@qq.com>
2020-09-21 20:34:22 +0800

    feat(widget):添加了会自行滚动的TextView控件

---
+++ app/src/main/java/com/gochiusa/musicapp/plus/widget/ScrollTextView.kt
@@ -0,0 +1,20 @@
+package com.gochiusa.musicapp.plus.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class ScrollTextView : AppCompatTextView {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int
+    ) : super(context, attrs, defStyleAttr)
+
+    override fun isFocused(): Boolean {
+        return true
+    }
+}
