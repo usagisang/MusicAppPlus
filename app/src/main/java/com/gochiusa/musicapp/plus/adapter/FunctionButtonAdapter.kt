030170f9866f98d3042fd1a53e39b25b1557c14a
Liu Zhengjie <1023192250@qq.com>
2020-10-06 21:09:30 +0800

    feat(adapter):添加显示功能按钮栏与搜索信息的适配器

---
+++ app/src/main/java/com/gochiusa/musicapp/plus/adapter/FunctionButtonAdapter.kt
@@ -0,0 +1,48 @@
+package com.gochiusa.musicapp.plus.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.gochiusa.musicapp.plus.R
+
+class FunctionButtonAdapter(val context: Context):
+    RecyclerView.Adapter<FunctionButtonAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(context)
+            .inflate(R.layout.item_main_function_button, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int = 4
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        when (position) {
+            0 -> {
+                holder.imageView.setImageResource(R.drawable.ic_main_download)
+                holder.textView.text = "下载管理"
+            }
+            1 -> {
+                holder.imageView.setImageResource(R.drawable.ic_main_local_music)
+                holder.textView.text = "本地音乐"
+            }
+            2 -> {
+                holder.imageView.setImageResource(R.drawable.ic_main_user_playlist)
+                holder.textView.text = "歌单收藏"
+            }
+            3 -> {
+                holder.imageView.setImageResource(R.drawable.ic_main_update_app)
+                holder.textView.text = "检查更新"
+            }
+        }
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.iv_main_button_item)
+        val textView: TextView = itemView.findViewById(R.id.tv_main_button_title)
+    }
+}
---
+++ app/src/main/java/com/gochiusa/musicapp/plus/adapter/SearchSongAdapter.kt
@@ -0,0 +1,39 @@
+package com.gochiusa.musicapp.plus.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.gochiusa.musicapp.plus.R
+import com.gochiusa.musicapp.plus.entity.Song
+import com.gochiusa.musicapp.plus.util.StringContract
+
+class SearchSongAdapter(list: MutableList<Song>, val context: Context) :
+    FootViewAdapter<Song, SearchSongAdapter.ContentViewHolder>(list) {
+
+    override fun createContentView(parent: ViewGroup?): NormalViewHolder {
+        val itemView = LayoutInflater.from(context).inflate(R.layout.item_adapter_music_card,
+            parent, false)
+        return ContentViewHolder(itemView)
+    }
+
+    override fun onBindContentViewHolder(holder: NormalViewHolder, position: Int) {
+        if (holder is ContentViewHolder) {
+            val song = getItem(position)
+            holder.songNameTextView.text = song.name
+            holder.singerNameTextView.text = song.artists.joinToString(
+                separator = StringContract.COMMA_SEPARATOR) {
+                it.name
+            }
+            holder.albumNameTextView.text = song.albumName
+        }
+    }
+
+    class ContentViewHolder(itemView: View) :
+        FootViewAdapter.NormalViewHolder(itemView, CONTENT_TYPE) {
+        val songNameTextView: TextView = itemView.findViewById(R.id.tv_adapter_music_name)
+        val singerNameTextView: TextView = itemView.findViewById(R.id.tv_adapter_singer_name)
+        val albumNameTextView: TextView = itemView.findViewById(R.id.tv_adapter_album_name)
+    }
+}
